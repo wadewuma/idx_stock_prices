7ed05a148d88c6c1024d4ee06dd8cb5d40fc89ec
Lukman Edwindra <lukman.edwindra@gmail.com>
2018-12-17 14:42:46 +0700

    Update idx_stock_prices.R

--- r_scripts/idx_stock_prices.R
+++ r_scripts/idx_stock_prices.R
@@ -1,5 +1,12 @@
-setwd('C:\\your\\path\\here')
-install.packages(c('BatchGetSymbols', 'rvest', 'xml2'))
+# setwd('C:\\your\\path\\here')
+
+if(!require(BatchGetSymbols))
+  install.packages('BatchGetSymbols')
+if(!require(rvest))
+  install.packages('rvest')
+if(!require(xml2))
+  install.packages('xml2')
+
 library(BatchGetSymbols)
 library(rvest)
 library(xml2)
